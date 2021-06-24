19fc7d87fd1d798c3eb41f79a845f0021425e0fd
erik <er99@family-rill.de>
2021-06-24 14:43:40 +0200

    Ergebnis Fragmente Lektion 6

--- app/src/main/java/com/example/android/layoutplayground/fragments/ConstraintFragment.kt
+++ app/src/main/java/com/example/android/layoutplayground/fragments/ConstraintFragment.kt
@@ -1,14 +1,11 @@
 package com.example.android.layoutplayground.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.android.layoutplayground.databinding.ConstraintFragmentBinding
-import com.example.android.layoutplayground.databinding.HorizontalFragmentBinding
 import com.example.android.layoutplayground.getRandomColor
 
 class ConstraintFragment: Fragment() {
